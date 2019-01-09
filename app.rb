0af7b1f1a4902ed36277c23bd8601d20b8889492
Eighto <eighto@cloud9.null>
2019-01-09 00:09:34 +0000

    prevent win by url entry

--- app.rb
+++ app.rb
@@ -66,13 +66,20 @@ class HangpersonApp < Sinatra::Base
   
   get '/win' do
     ### YOUR CODE HERE ### -- prevent manual entry of /win on to url to result in a win
-    
-    erb :win # You may change/remove this line
+    if @game.check_win_or_lose == :win
+      erb :win # You may change/remove this line
+    else
+      erb :show
+    end
   end
   
   get '/lose' do
     ### YOUR CODE HERE ###
-    erb :lose # You may change/remove this line
+    if @game.check_win_or_lose == :lose
+      erb :lose # You may change/remove this line
+    else
+      erb :show
+    end
   end
   
 end
